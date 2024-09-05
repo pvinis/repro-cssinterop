db2d6b0c7214009f8b1a61f4c6e6a5cb01266afd
Pavlos Vinieratos <pvinis@gmail.com>
2024-09-05 12:45:51 +0300

    boom

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,9 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import { BackButton } from '../components/BackButton';
 import Details from '../screens/details';
 import Overview from '../screens/overview';
+import { cssInterop } from 'nativewind';
 
 export type RootStackParamList = {
   Overview: undefined;
@@ -12,17 +12,39 @@ export type RootStackParamList = {
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-export default function RootStack() {
+export function RootStackOld() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Overview">
-        <Stack.Screen name="Overview" component={Overview} />
         <Stack.Screen
-          name="Details"
-          component={Details}
-          options={({ navigation }) => ({
-            headerLeft: () => <BackButton onPress={navigation.goBack} />,
-          })}
+          name="Overview"
+          component={Overview}
+          options={{
+            headerTitleStyle: { color: 'green' },
+            headerStyle: { backgroundColor: 'red' },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+cssInterop(Stack.Screen, {
+  headerClassName: 'options.headerStyle',
+});
+
+export default function RootStackNew() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Overview">
+        <Stack.Screen
+          name="Overview"
+          component={Overview}
+          options={{
+            headerTitleStyle: { color: 'green' },
+            // headerStyle: { backgroundColor: 'red' },
+          }}
+          headerClassName="bg-red-800"
         />
       </Stack.Navigator>
     </NavigationContainer>
